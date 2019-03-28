dafc6e5b7c2d73fef53d7ff00f3955f96d249924
Hoang Minh Thanh <sieusaochemgio@gmail.com>
2019-03-28 09:24:13 +0700

    Updates

---
+++ precache-manifest.6e5a4e63350fa73de19355f31001c91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa0d85a6b1f2dbafa8db",
+    "url": "/static/js/main.d25b2c89.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "fa0d85a6b1f2dbafa8db",
+    "url": "/static/css/main.1c8551d2.chunk.css"
+  },
+  {
+    "revision": "d5e4cc47be3287fe0be266bdfcc0feb0",
+    "url": "/index.html"
+  }
+];
