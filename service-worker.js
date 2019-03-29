43ac5ed5e562d835c3c57e2630ade0e5113e0feb
Hoang Minh Thanh <sieusaochemgio@gmail.com>
2019-03-29 10:17:50 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/giaithuat.github.io/precache-manifest.93defba855684fa7c963906b44bec913.js"
+  "/precache-manifest.ca7b84d34720277526fe403aaf030c21.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/giaithuat.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
