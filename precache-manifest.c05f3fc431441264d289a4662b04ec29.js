fa79ef217b759987d25f32f6531717664230b8e7
hmthanh <hmthanhgm@gmail.com>
2020-03-24 18:44:27 +0700

    update new file and react file without map

---
+++ precache-manifest.c05f3fc431441264d289a4662b04ec29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd2552cc5e31cecd27b510a75e607a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3b93c75438de26ad879",
+    "url": "/static/css/main.8ac000d7.chunk.css"
+  },
+  {
+    "revision": "9445a548a1e16cb06e32",
+    "url": "/static/js/2.5fb6576e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5fb6576e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66cb6eb7fa5242241be",
+    "url": "/static/js/3.9c5bbe3c.chunk.js"
+  },
+  {
+    "revision": "e3b93c75438de26ad879",
+    "url": "/static/js/main.bd068c13.chunk.js"
+  },
+  {
+    "revision": "76fe6868236f7f33f029",
+    "url": "/static/js/runtime-main.3c907668.js"
+  }
+]);
