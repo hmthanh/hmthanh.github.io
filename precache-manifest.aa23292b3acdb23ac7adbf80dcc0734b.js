d00a953803c71f8462951f18e5bd4e3fed2ab7f9
hmthanh <hmthanhgm@gmail.com>
2020-11-02 01:37:33 +0700

    add Minh-Thanh's Blog

---
+++ precache-manifest.aa23292b3acdb23ac7adbf80dcc0734b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ec463fbc4cdfb4b015d6fff6a10af9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ead954182af71b3e0699",
+    "url": "/static/css/main.c5395b08.chunk.css"
+  },
+  {
+    "revision": "d6952c3a8c4a3b9d9800",
+    "url": "/static/js/0.d3ae0e45.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/0.d3ae0e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ae2981ec269e2a13ff",
+    "url": "/static/js/1.17876256.chunk.js"
+  },
+  {
+    "revision": "2378aa89d984f23843f4",
+    "url": "/static/js/10.24e0f2f0.chunk.js"
+  },
+  {
+    "revision": "6b3a79bdf390f70208d0",
+    "url": "/static/js/11.68e28da1.chunk.js"
+  },
+  {
+    "revision": "6f4d168b3b38643cf46d",
+    "url": "/static/js/12.449d4534.chunk.js"
+  },
+  {
+    "revision": "899dfb326aa8280a5f02",
+    "url": "/static/js/13.c580bcc4.chunk.js"
+  },
+  {
+    "revision": "21a20f3330d39ec6dda6",
+    "url": "/static/js/14.d3925bfd.chunk.js"
+  },
+  {
+    "revision": "9bcb8d57b32eaf6403cd",
+    "url": "/static/js/15.a0069c6b.chunk.js"
+  },
+  {
+    "revision": "420e5777af222f862859",
+    "url": "/static/js/5.af096a47.chunk.js"
+  },
+  {
+    "revision": "fa70ab308112505e01d9",
+    "url": "/static/js/6.18058dfb.chunk.js"
+  },
+  {
+    "revision": "c50c771658c38c7c1fcfc5b118622f46",
+    "url": "/static/js/6.18058dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6155ec28c8956f771300",
+    "url": "/static/js/7.d0f1cd21.chunk.js"
+  },
+  {
+    "revision": "8f49b17c350070bab04f",
+    "url": "/static/js/8.cbe019bc.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/8.cbe019bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10f0d85fa8b9f629282",
+    "url": "/static/js/9.5b3f4633.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/static/js/9.5b3f4633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead954182af71b3e0699",
+    "url": "/static/js/main.29bec271.chunk.js"
+  },
+  {
+    "revision": "5fb9e3aa3872450cd764",
+    "url": "/static/js/runtime-main.33ce8520.js"
+  },
+  {
+    "revision": "f4b7f787db86064f5405",
+    "url": "/static/js/xlsx.fa972cd7.chunk.js"
+  }
+]);
