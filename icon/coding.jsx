1761c84899b469ff709eb3899b1f115db6dd0255
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-03 01:08:15 +0700

    icons

---
+++ icon/coding.jsx
@@ -0,0 +1,102 @@
+export const Coding = (props) => (
+  <svg {...props} viewBox="0 0 60 60">
+    <g>
+      <path
+        fill="#97aab2"
+        d="M44 55v1H16v-1s6-1 6-7v-1h16v1c0 6 6 7 6 7z"
+        opacity="1"
+      ></path>
+      <rect
+        width="32"
+        height="4"
+        x="14"
+        y="55"
+        fill="#b3c1c9"
+        rx="2"
+        opacity="1"
+      ></rect>
+      <path
+        fill="#b3c1c9"
+        d="M59 41v4c0 1.66-1.34 3-3 3H4c-1.66 0-3-1.34-3-3v-4l1-2h56z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#18316d"
+        d="M59 4v37H1V4c0-1.66 1.34-3 3-3h52c1.66 0 3 1.34 3 3z"
+        opacity="1"
+      ></path>
+      <g fill="#fff">
+        <path
+          d="M24 17a.997.997 0 0 1-.707-.293l-5-5a.999.999 0 0 1 0-1.414l5-5a.999.999 0 1 1 1.414 1.414L20.414 11l4.293 4.293A.999.999 0 0 1 24 17zM36 17a.999.999 0 0 1-.707-1.707L39.586 11l-4.293-4.293a.999.999 0 1 1 1.414-1.414l5 5a.999.999 0 0 1 0 1.414l-5 5A.997.997 0 0 1 36 17zM28 18a1 1 0 0 1-.948-1.317l4-12a1 1 0 1 1 1.897.633l-4 12a1 1 0 0 1-.948.684zM8 22H7a1 1 0 1 1 0-2h1a1 1 0 1 1 0 2z"
+          fill="#ffffff"
+          opacity="1"
+        ></path>
+      </g>
+      <path
+        fill="#4ed8d5"
+        d="M28 22H13a1 1 0 1 1 0-2h15a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#ff4d78"
+        d="M39 22h-7a1 1 0 1 1 0-2h7a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#ffffff"
+        d="M8 27H7a1 1 0 1 1 0-2h1a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#f9dc4e"
+        d="M31 27H16a1 1 0 1 1 0-2h15a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#4ed8d5"
+        d="M44 27h-9a1 1 0 1 1 0-2h9a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#ffffff"
+        d="M8 32H7a1 1 0 1 1 0-2h1a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#4ed8d5"
+        d="M28 32H13a1 1 0 1 1 0-2h15a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#ff4d78"
+        d="M39 32h-7a1 1 0 1 1 0-2h7a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#ffffff"
+        d="M8 37H7a1 1 0 1 1 0-2h1a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#ff4d78"
+        d="M53 27h-5a1 1 0 1 1 0-2h5a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#f9dc4e"
+        d="M31 37H16a1 1 0 1 1 0-2h15a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#4ed8d5"
+        d="M44 37h-9a1 1 0 1 1 0-2h9a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+      <path
+        fill="#ff4d78"
+        d="M53 37h-5a1 1 0 1 1 0-2h5a1 1 0 1 1 0 2z"
+        opacity="1"
+      ></path>
+    </g>
+  </svg>
+);
---
+++ icon/index.js
@@ -0,0 +1,27 @@
+import { ArrowaltCircleUp } from "./arrowaltcircleup.jsx"
+import { Envelope } from "./envelope.jsx"
+import { Facebook } from "./facebook.jsx"
+import { Github } from "./github.jsx"
+import { HackerRank } from "./hackerrank.jsx"
+import { Kaggle } from "./kaggle.jsx"
+import { Linkedin } from "./linkedin.jsx"
+import { Medium } from "./medium.jsx"
+import { Phone } from "./phone.jsx"
+import { Skype } from "./skype.jsx"
+import { Stackoverflow } from "./stackoverflow.jsx"
+import { Twitter } from "./twitter.jsx"
+
+export {
+  ArrowaltCircleUp,
+  Envelope,
+  Facebook,
+  Github,
+  HackerRank,
+  Kaggle,
+  Linkedin,
+  Medium,
+  Phone,
+  Skype,
+  Stackoverflow,
+  Twitter,
+}
