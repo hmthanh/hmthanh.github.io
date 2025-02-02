128b12bf57014503bd84e38598c060a992ce041e
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-03 03:50:10 +0700

    update new time loading animation

--- app/page.js
+++ app/page.js
@@ -16,18 +16,46 @@ import Loading from '@/components/Loading';
 
 export default function Page() {
   const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   const handleLoad = () => {
+  //     setTimeout(() => {
+  //       setIsLoading(false);
+  //     }, 1000);
+  //     //   setIsLoading(false);
+  //   };
+
+  //   window.addEventListener('load', handleLoad);
+
+  //   return () => {
+  //     window.removeEventListener('load', handleLoad);
+  //   };
+  // }, []);
   useEffect(() => {
+    let timeoutId;
+
     const handleLoad = () => {
-      setTimeout(() => {
+      // If the page loads in less than 3 seconds, wait for 1 second before hiding the loader
+      if (timeoutId) {
+        clearTimeout(timeoutId); // Clear the 3-second timeout
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 800); // Wait for 1 second
+      } else {
+        // If the page takes more than 3 seconds to load, hide the loader immediately
         setIsLoading(false);
-      }, 1000);
-      //   setIsLoading(false);
+      }
     };
+
+    // Set a timeout for 3 seconds
+    timeoutId = setTimeout(() => {
+      timeoutId = null; // Clear the timeout ID
+    }, 1000);
 
     window.addEventListener('load', handleLoad);
 
     return () => {
       window.removeEventListener('load', handleLoad);
+      if (timeoutId) clearTimeout(timeoutId); // Cleanup the timeout
     };
   }, []);
 
